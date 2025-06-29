6b02018dcb99b8a727417a543cdc1738f5341c78
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-06-29 18:41:35 -0500

    Establish the chat manager

--- chat_manager.py
+++ chat_manager.py
@@ -5,10 +5,14 @@ from rich.console import Console
 from chat_models import ChatConversationHistory
 from config import DEFAULT_MODEL
 from ollama_client import OllamaClient
-from rick.markdown import Markdown
+from rich.markdown import Markdown
 
 class ChatManager:
-    client: OllamaClient = None
-    model: str = DEFAULT_MODEL
-    history: ChatConversationHistory = ChatConversationHistory()
-    console: Console = Console()
+    client: OllamaClient | None
+    model: str
+
+    def __init__(self, model: str = DEFAULT_MODEL):
+        self.client: OllamaClient | None = None
+        self.model = model
+        self.history = ChatConversationHistory()
+        self.console = Console()
